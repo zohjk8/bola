c0d342586b19966da17bf36aecb0c470d5060a34
zohjk8 <40482512+zohjk8@users.noreply.github.com>
2018-06-22 22:49:50 +0700

    Add files via upload

---
+++ bola.js
@@ -0,0 +1,11 @@
+//<![CDATA[
+   function server1() {
+      document.getElementById(&quot;tivi&quot;).innerHTML = &#39;<iframe allowfullscreen='true' allowtransparency='true' frameborder='0' height='100%' scrolling='no' src='//www.bolamaniax.com/2018/01/bein-sport-0.html' width='100%'/>&#39;;
+   }
+   function server2() {
+      document.getElementById(&quot;tivi&quot;).innerHTML = &#39;<iframe allowfullscreen='true' allowtransparency='true' frameborder='0' height='100%' scrolling='no' src='//www.bolamaniax.com/2018/01/bein-sport-2.html' width='100%'/>&#39;;
+   }
+   function server3() {
+      document.getElementById(&quot;tivi&quot;).innerHTML = &#39;<iframe allowfullscreen='true' allowtransparency='true' frameborder='0' height='100%' scrolling='no' src='//www.bolamaniax.com/2018/01/bein-sport-3.html' width='100%'/>&#39;;
+   }
+//]]>
